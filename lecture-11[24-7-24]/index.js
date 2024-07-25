61d010fc126b2e2270e7dc583e84fc8a10021f19
Yogesh Tomar <kshanu385@gmail.com>
2024-07-25 07:05:20 +0530

    Promises-2

---
+++ lecture-11[24-7-24]/index.js
@@ -0,0 +1,17 @@
+// const ghData = fetch("https://api.github.com/users");
+// console.log(ghData);
+
+// const users = Promise.resolve(ghData);
+
+// users
+//   .then((Response) => {
+//     return Response;
+//   })
+//   .then((value) => {
+//     console.log(value.json());
+//   })
+//   .then((value) => {
+//     console.log(value);
+//   });
+
+
