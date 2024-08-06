a583b22f127d3fe0b300275d3dd4568fefa88f0e
Yogesh Tomar <kshanu385@gmail.com>
2024-08-06 09:40:26 +0530

    React 1

---
+++ reactJS[5-8-24]/Calculator/src/App.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1><a href="https://www.google.com">First Recat APP</a></h1>
+      <p className="" htmlFor="">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora,
+        temporibus.
+      </p>
+    </div>
+  );
+}
+
+export default App;
